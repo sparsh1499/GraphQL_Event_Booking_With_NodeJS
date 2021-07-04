0aa9d1bf7df822752f1382bca7eae79d7d09261e
Sparsh Bhardwaj <Sparsh.Bhardwaj@zimozi.co>
2021-07-04 14:23:13 +0530

    Initial Changes

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const {graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/graphql", graphqlHTTP ({
+    schema: buildSchema(`
+        type RootQuery{
+            events : [String!]!
+        }
+
+        type RootMutation{
+            createEvent(name : String):String
+
+        }
+
+        schema{
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return [
+                "NodeJS", 
+                "Python",
+                "Java",
+                "HTML",
+                "CSS",
+                "Bootstrap",
+                "GraphQL",
+                "Amazon AWS"]
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}))
+
+app.listen(3000);
